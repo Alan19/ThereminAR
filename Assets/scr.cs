8eaa03092a530c6481b450da3f511c9d4fad0e58
Alan19 <Alan19@users.noreply.github.com>
2019-03-31 23:27:56 -0400

    Added some fields

--- Assets/scr.cs
+++ Assets/scr.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.MagicLeap;
 
 public class scr : MonoBehaviour {
 
+    private MLInputController _controller;
+    public GameObject tempoBox;
+    private GameObject instrumentToggle;
+    private GameObject currentNote;
+    private GameObject metronomeButton;
     int tempo = 0;
     string key = "A";
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        tempoBox = GameObject.Find("Cube");
+        instrumentToggle = GameObject.Find("Cube (1)");
+        currentNote = GameObject.Find("Cube (2)");
+        metronomeButton = GameObject.Find("Cylinder");
+    }
 	
 	// Update is called once per frame
 	void Update () {
